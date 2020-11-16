4a498540dc9b5f37af60113478931b92563b69bb
Melina Nadal <melinanhopkins@gmail.com>
2020-11-16 15:43:30 -0500

    css update

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// IPO - Input Process Output 
+
+// Constants and variables (State)
+let skills;
+// Cached Element References
+const $button = $('button');
+const $ul = $('ul');
+const $input = $('input');
+// Event Listeners
+$button.on('click', handleAddSkill);
+// Functions
+init();
+
+function init() {
+    skills = [];
+    render();
+}
+function handleAddSkill(){
+    // store the value from the input tag inside a local variable
+    const skill = $input.val();
+    // check to make sure we have skill data
+    if(skill) {
+        
+        
+  
+    // create the UI for a skill
+    const $skill =$(`<li><span>X</span>${skill}</li>`)
+    //  push the skill UI inside a skills list
+    skills.push($skill);
+    //  clear the input tag's value
+    $input.val('');
+    // call render() 
+    render()
+} else return;
+}
+function handleDelete() {
+    alert('Elements inside of ul tag clicked');
+}
+
+function render() {
+// to take the list of the skills array and add them to UI
+if(!skills.length) $ul.css('margin-bottom', '30px');
+else $ul.css('margin-bottom', '0px');
+
+$ul.html(skills);
+}
